a41f32cc7e2c92ebb976dbd90572ba6e7a958a45
Brandon Slaa <branslaa@gmail.com>
2017-04-05 11:20:58 -0400

    Adding backend

--- WarCardGame/WarCardGame/LogonWindow.xaml.cs
+++ WarCardGame/WarCardGame/LogonWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WarCardGame.ServiceReference1;
+using WarCardGameLibrary;
 
 namespace WarCardGame
 {
@@ -19,6 +21,7 @@ namespace WarCardGame
 	/// </summary>
 	public partial class LogonWindow : Window
 	{
+		private string name = "";
 		public LogonWindow()
 		{
 			InitializeComponent();
--- WarCardGame/WarCardGame/MainWindow.xaml.cs
+++ WarCardGame/WarCardGame/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,14 +13,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WarCardGameLibrary;
+using WarCardGame.ServiceReference1;
+
 
 namespace WarCardGame
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	[CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant, UseSynchronizationContext = false)]
+	public partial class MainWindow : Window, WarCardGame.ServiceReference1.IUserCallback
 	{
+		private Deck deck = new Deck();
+		private UserClient gameBrd = null;
+		private string name = "";
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -27,12 +35,79 @@ namespace WarCardGame
 
 		private void btnPlayNextCard_Click(object sender, RoutedEventArgs e)
 		{
-
+			bool canbePlayed = gameBrd.CanBePlayed(name);
+			if (canbePlayed)
+			{
+				try
+				{
+					Card card = deck.Draw();
+					string c = card.Name;
+					gameBrd.PostMessage(name + " has played a " + c);
+					string winner = gameBrd.addCard(name, card);
+					if (winner != "")
+						gameBrd.PostMessage(winner);
+					
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			}
+			else
+			{
+				gameBrd.PostMessage("You have already played a card");
+			}
 		}
 
 		private void btnViewScores_Click(object sender, RoutedEventArgs e)
 		{
 
 		}
+
+		private void connectToMessageBoard()
+		{
+			try
+			{
+				gameBrd = new UserClient(new InstanceContext(this));
+
+				if (gameBrd.Join(textAlias.Text))
+				{
+					// Alias accepted by the service so update GUI
+					listMessages.ItemsSource = gameBrd.GetAllMessages();
+					textAlias.IsEnabled = buttonSet.IsEnabled = false;
+				}
+				else
+				{
+					// Alias rejected by the service so nullify service proxies
+					gameBrd = null;
+					MessageBox.Show("ERROR: Alias in use. Please try again.");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private delegate void GuiUpdateDelegate(string[] messages);
+
+		public void SendAllMessages(string[] messages)
+		{
+			if (this.Dispatcher.Thread == System.Threading.Thread.CurrentThread)
+			{
+				try
+				{
+					listMessages.ItemsSource = messages;
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			}
+			else
+				this.Dispatcher.BeginInvoke(new GuiUpdateDelegate(SendAllMessages), new object[] { messages });
+		}
+
+
 	}
 }
